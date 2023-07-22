cd66ae5d8136157260cfc0198b3a4258e7eba26f
Anogh Dalal <anogh25@gmail.com>
2023-07-22 11:29:11 +0530

    first commit

--- api/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authController = require("../controllers/auth");
-
-const router = express.Router();
-
-router.route("/register").post(authController.register);
-router.route("/login").post(authController.login);
-
-module.exports = router;
